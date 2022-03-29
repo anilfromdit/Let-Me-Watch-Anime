dedccbbee598f970fc39367f177d509ba8130149
anilfromdit <anilfromdit@gmail.com>
2022-03-29 12:52:22 +0530

    added check if user is on supported site or not

--- main.js
+++ main.js
@@ -1,51 +1,58 @@
 
-chrome.commands.onCommand.addListener((command) => {
-    getCurrentTab();
-    if (command === 'next') {
-        console.log("Started Next");
-        getCurrentTab(next)
-    }
-    else if (command === 'previous') {
-        console.log("Started Previous");
-        getCurrentTab(previous)
-    }
-});
+chrome.webNavigation.onCompleted.addListener(function () {
+    console.log("loaded")
 
-async function getCurrentTab(myCallBackFunction) {
-    let queryOptions = { active: true, currentWindow: true };
-    let [tab] = await chrome.tabs.query(queryOptions);
-    const newUrl = tab.url;
-    const finalUrl = newUrl.split("episode-");
-    let animeLink = finalUrl[0] + "episode-";
-    let episodeNumber = parseInt(finalUrl[1], 10)
-    let rest = finalUrl[1].split(episodeNumber)
-    myCallBackFunction(animeLink,episodeNumber,rest[1])
-    console.log(rest)
-    
 
-}
-function next(animeLink,episodeNumber,rest) {
-    let ultraMaxProFinalLink;
-    if(rest){
-        ultraMaxProFinalLink = animeLink + (episodeNumber + 1) + rest;}
-        else{
-         ultraMaxProFinalLink = animeLink + (episodeNumber + 1);
+    chrome.commands.onCommand.addListener((command) => {
+        if (command === 'next') {
+            console.log("Started Next");
+            getCurrentTab(next)
         }
-    console.log(ultraMaxProFinalLink)
-    chrome.tabs.update({ active: true, url: ultraMaxProFinalLink });
-
-}
-function previous(animeLink,episodeNumber,rest) {
-   let ultraMaxProFinalLink;
-    if(episodeNumber<=1){
-        return;
+        else if (command === 'previous') {
+            console.log("Started Previous");
+            getCurrentTab(previous)
+        }
+    });
+    
+    async function getCurrentTab(myCallBackFunction) {
+        let queryOptions = { active: true, currentWindow: true };
+        let [tab] = await chrome.tabs.query(queryOptions);
+        const newUrl = tab.url;
+        const finalUrl = newUrl.split("episode-");
+        let animeLink = finalUrl[0] + "episode-";
+        let episodeNumber = parseInt(finalUrl[1], 10)
+        let rest = finalUrl[1].split(episodeNumber)
+        myCallBackFunction(animeLink, episodeNumber, rest[1])
+    
+    
     }
-    if(rest){
-        ultraMaxProFinalLink = animeLink + (episodeNumber - 1) + rest;}
-    else{
-        ultraMaxProFinalLink = animeLink + (episodeNumber - 1);
+    function next(animeLink, episodeNumber, rest) {
+        let ultraMaxProFinalLink;
+        if (rest) {
+            ultraMaxProFinalLink = animeLink + (episodeNumber + 1) + rest;
+        }
+        else {
+            ultraMaxProFinalLink = animeLink + (episodeNumber + 1);
+        }
+        console.log(ultraMaxProFinalLink)
+        chrome.tabs.update({ active: true, url: ultraMaxProFinalLink });
+    
     }
-    console.log(ultraMaxProFinalLink)
-    chrome.tabs.update({ active: true, url: ultraMaxProFinalLink });
+    function previous(animeLink, episodeNumber, rest) {
+        let ultraMaxProFinalLink;
+        if (episodeNumber <= 1) {
+            return;
+        }
+        if (rest) {
+            ultraMaxProFinalLink = animeLink + (episodeNumber - 1) + rest;
+        }
+        else {
+            ultraMaxProFinalLink = animeLink + (episodeNumber - 1);
+        }
+        console.log(ultraMaxProFinalLink)
+        chrome.tabs.update({ active: true, url: ultraMaxProFinalLink });
+    
+    }
 
-}
+
+}, { url: [{ hostSuffix: "animeheaven.pro" },{ hostSuffix: "animeland.tv" }] });
