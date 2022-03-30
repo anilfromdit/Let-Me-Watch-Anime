9e75d7d9b98653f68ff1605f14fe9b9f7d6cf2c6
anilfromdit <anilfromdit@gmail.com>
2022-03-30 12:08:00 +0530

    added support for animeheaven.online.. will design html in the end

--- main.js
+++ main.js
@@ -1,30 +1,39 @@
 
 chrome.webNavigation.onCompleted.addListener(function () {
-    console.log("loaded")
-
-
+    console.log("Ready!!!")
     chrome.commands.onCommand.addListener((command) => {
         if (command === 'next') {
-            console.log("Started Next");
             getCurrentTab(next)
         }
         else if (command === 'previous') {
-            console.log("Started Previous");
             getCurrentTab(previous)
         }
     });
-    
     async function getCurrentTab(myCallBackFunction) {
+        let finalUrl, animeLink, episodeNumber, rest, tRest;
         let queryOptions = { active: true, currentWindow: true };
+
         let [tab] = await chrome.tabs.query(queryOptions);
         const newUrl = tab.url;
-        const finalUrl = newUrl.split("episode-");
-        let animeLink = finalUrl[0] + "episode-";
-        let episodeNumber = parseInt(finalUrl[1], 10)
-        let rest = finalUrl[1].split(episodeNumber)
-        myCallBackFunction(animeLink, episodeNumber, rest[1])
-    
-    
+
+
+        if (newUrl.includes("animeheaven.online")) {
+            finalUrl = newUrl.split("/");
+            episodeNumber = parseInt(finalUrl[finalUrl.length - 1])
+            animeLink = finalUrl[0];
+            for (let i = 1; i < (finalUrl.length - 1); i++) {
+                animeLink = animeLink + finalUrl[i] + "/"
+            }
+        }
+        else {
+            finalUrl = newUrl.split("episode-");
+            animeLink = finalUrl[0] + "episode-";
+            episodeNumber = parseInt(finalUrl[1], 10)
+            tRest = finalUrl[1].split(episodeNumber)
+            rest = tRest[1]
+        }
+        // console.log("\nanimeLink: " + animeLink + "\nepisodeNumber: " + episodeNumber + "\nrest: " + rest)
+        myCallBackFunction(animeLink, episodeNumber, rest)
     }
     function next(animeLink, episodeNumber, rest) {
         let ultraMaxProFinalLink;
@@ -36,7 +45,7 @@ chrome.webNavigation.onCompleted.addListener(function () {
         }
         console.log(ultraMaxProFinalLink)
         chrome.tabs.update({ active: true, url: ultraMaxProFinalLink });
-    
+
     }
     function previous(animeLink, episodeNumber, rest) {
         let ultraMaxProFinalLink;
@@ -51,8 +60,8 @@ chrome.webNavigation.onCompleted.addListener(function () {
         }
         console.log(ultraMaxProFinalLink)
         chrome.tabs.update({ active: true, url: ultraMaxProFinalLink });
-    
+
     }
 
 
-}, { url: [{ hostSuffix: "animeheaven.pro" },{ hostSuffix: "animeland.tv" }] });
+}, { url: [{ hostSuffix: "animeheaven.pro" }, { hostSuffix: "animeland.tv" }, { hostContains: "animeheaven.online" }] });
